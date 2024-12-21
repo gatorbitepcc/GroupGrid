fdb2c863d76297fd8d92ae294db92cc425def265
gatorbitepcc <165971315+gatorbitepcc@users.noreply.github.com>
2024-12-21 09:01:19 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript to handle the "Get Started" button
+document.getElementById('signupBtn').addEventListener('click', function() {
+    window.location.href = "signup.html"; // Redirect to signup page
+});
