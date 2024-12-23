fb9cbc9b7a700254a2935007f5e160b4f0cd4407
gatorbitepcc <165971315+gatorbitepcc@users.noreply.github.com>
2024-12-23 14:22:18 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@
 document.getElementById('signupBtn').addEventListener('click', function() {
     window.location.href = "sign_up.html"; // Redirect to signup page
 });
+document.getElementById('logBTn').addEventListener('click', function() {
+    window.location.href = "login.html"; // Redirect to signup page
+});
